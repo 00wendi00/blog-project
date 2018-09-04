0b3c58757d8758e2ddfc471eb7289aff07c3b7fe
wendi <842876912@qq.com>
2018-09-04 23:54:49 +0800

    允许使用root用户启动celery

--- blog_project/celery.py
+++ blog_project/celery.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import
 
 import os
 
-from celery import Celery
+from celery import Celery, platforms
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
@@ -23,6 +23,8 @@ app = Celery('blog')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+platforms.C_FORCE_ROOT = True
 
 
 @app.task(bind=True)
